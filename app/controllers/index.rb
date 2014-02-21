b445dcfd638c6712ccc825531b93a6a93bf5e4c2
thejessleigh <j.l.unrein@gmail.com>
2014-02-21 16:20:48 -0600

    adjusted the home and deck views. Added logout function. Unless user is logged in they are not allowed to access the rest of the site.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,13 @@
 get '/' do
+  if session[:status]
+    @user = User.find(session[:user_id])
+  end
   erb :index
+end
+
+get '/log_out/' do
+  session.clear
+  redirect '/'
 end
 
 get '/decks/' do
