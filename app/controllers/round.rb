596fec9d71d787c2b9ca284e643dc6a8d647a7a0
thejessleigh <j.l.unrein@gmail.com>
2014-02-21 15:26:17 -0600

    Logic working to move through pages
    We can now create a deck, store the cards in sessions, go through the cards, and on the last one go to a stats page. The navigation logic works, now we just need to incorporate the logic for counting right and wrong answers and displaying stats.

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,16 +1,33 @@
-get '/round/:deck_id' do
-  @deck = Deck.find(params[:deck_id])
-  @cards = @deck.cards.shuffle
-  erb :start_round
-end
-
-get '/round/card/:card_id ' do
+get '/round/card/:card_id' do
   @card = Card.find(params[:card_id])
   erb :run_card
 end
 
-get '/card/answer/' do
+post '/card/answer/:card_id' do
   @guess = Guess.create
+  if session[:cards].empty?
+    redirect '/round/stats'
+  else
+    redirect "/round/card/#{session[:cards].pop.id}"
+  end
+end
+
+get '/round/stats' do
+  erb :stats
+end
+
+get '/round/:deck_id' do
+  @deck = Deck.find(params[:deck_id])
+  @round = Round.create(deck_id: params[:deck_id], user_id: session[:user_id])
+  session[:round_id] = @round.id
+  session[:cards] = @deck.cards.shuffle
+  session[:deck_title] =@deck.title
+  # binding.pry
+  erb :start_round
 end
 
 
+## card answer final redirects to view stats
+
+## post card/answer and card/answer/final up the counter in guess
+## for correct answers and do nothing for incorrect answers.
